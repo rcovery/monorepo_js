97b03ee26e3a2cc93d18df1596ac589fc53991d1
rpereiradt <r.pereira@dtnetwork.com.br>
2023-02-13 09:38:57 -0300

    base eslint config

--- projects/web/.eslintrc.js
+++ projects/web/.eslintrc.js
@@ -1,3 +1,6 @@
+const eslintDefaults = require('@onlytests/eslint/index');
+
 module.exports = {
+  ...eslintDefaults,
   extends: 'next/core-web-vitals',
 };
--- setup/eslint/index.js
+++ setup/eslint/index.js
@@ -1,7 +1,15 @@
 module.exports = {
+  plugins: ["@typescript-eslint/eslint-plugin"],
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:prettier/recommended",
   ],
+  ignorePatterns: [".eslintrc.js"],
+  rules: {
+    "@typescript-eslint/interface-name-prefix": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+  },
 };
