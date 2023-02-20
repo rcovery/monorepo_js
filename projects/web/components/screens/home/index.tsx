e4a6471a5049df425a98db76fcb4f3fbd91d9197
rpereiradt <r.pereira@dtnetwork.com.br>
2023-02-20 09:52:10 -0300

    Criando página home para acessar o backend

--- projects/web/components/screens/home/index.tsx
+++ projects/web/components/screens/home/index.tsx
@@ -1,5 +1,37 @@
+import { useState } from 'react';
+import axios from 'axios';
+
+async function getApp() {
+  const response = await axios.get('http://localhost:3001/');
+
+  return {
+    status: response.status ?? 500,
+    text: response.data ?? '',
+  };
+}
+
 function Page() {
-  return <h1>Hello!</h1>;
+  const [apiResponse, setApiResponse] = useState(null);
+  function makeRequest() {
+    getApp().then((data) => {
+      setApiResponse(data);
+    });
+  }
+  return (
+    <>
+      <h1>Hello!</h1>
+      <button onClick={() => makeRequest()}>
+        Click here to make a request!
+      </button>
+      <hr />
+      {apiResponse ? (
+        <>
+          <h4>Status: {apiResponse.status}</h4>
+          <p>Text: {apiResponse.text}</p>
+        </>
+      ) : null}
+    </>
+  );
 }
 
 export default Page;
